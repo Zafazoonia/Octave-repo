8cc63a03b8952e1f49e0b170ba80c7b13a9266e6
Zafazoonia <gk.krishna737@gmail.com>
2023-01-28 12:39:41 +0530

    file_saving_in_plotter

--- Plotter.m
+++ Plotter.m
@@ -1,4 +1,4 @@
-function plotter(filename)
+function Plotter(filename)
   M=dlmread(filename);
   for i=1:size(M)(1)
     if M(i,1)>0
@@ -7,11 +7,13 @@ function plotter(filename)
   endfor
   L=i;
   U=size(M)(1);
-  Sub=M(L:U,1:2);
-` subx=Sub(:,1);
-  suby=Sub(:,2);
+  Sub=M(L:U,1:3);
+  subx=Sub(:,1);
+  suby=Sub(:,2).*Sub(:,3);
+  f=figure();
   plot(subx,suby);
   xlabel("2theta");
   ylabel("Intensity");
-  
+  print (f, filename, "-dpng");
+
 endfunction
