402986b8955375cafca8b177492c5bb3ba3bceb3
Zafazoonia <37117104+Zafazoonia@users.noreply.github.com>
2023-03-16 16:28:31 +0530

    Update XRRplot.m
    xrr fft

--- XRRplot.m
+++ XRRplot.m
@@ -1,19 +1,35 @@
-function XRRPlotter(filename)
+function xrr(filename, lwr=0, upr=10.0)
   M=dlmread(filename);
-  for i=1:size(M)(1)
-    if M(i,1)>0
-      break;
-    endif
-  endfor
-  L=i;
-  U=size(M)(1);
-  Sub=M(L:U,1:3);
-  subx=Sub(:,1);
-  suby=Sub(:,2).*Sub(:,3);
-  f=figure();
-  semilogy(subx,suby);
-  xlabel("2theta");
-  ylabel("Intensity");
-  print (f, filename, "-dpng");
+  Sub=M(:,1);
+  L=find(Sub==lwr);
+  U=find(Sub==upr);
+  %U=size(M)(1)-2;
+  %U=551;
+  Subx=M(L:U,1);
+  Suby0=M(L:U,2);
+  Suby1=M(L:U,3);
+  Suby=((Suby0.*Suby1));
+  h=figure("visible","on");
+  %dx = Subx(2)-Subx(1);
 
+
+
+  subplot(2,1,1);
+semilogy(Subx,Suby);
+ subplot(2,1,2);
+ SubFFT=abs(fft(Suby));
+  [pks idx] = findpeaks(SubFFT,"MinPeakDistance",10);
+  k=1:1:size(SubFFT)(1);
+ plot(k',SubFFT,k'(idx),SubFFT(idx),"xm");
+  %plot(k',SubFFT);
+  for i=1:size(idx)
+    text(k'(idx(i)),pks(i),num2str(k'(idx(i))),"fontsize",9);
+  endfor
+  %text (Sub, 0.7, "arbitrary text");
+  xlabel("2-theta (degrees)");
+  ylabel("Intensity (counts) ");
+  %[filepath,name,ext] = fileparts(filename);
+  printname=filename(1:(size(filename)(2)-4));
+  title({"XRD";printname});
+  print(h,printname,"-dpng");
 endfunction
